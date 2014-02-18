3987b58922b9f13262b7652ffe9f70a80eb045de
guinardA <guinard.arnaud@gmail.com>
2014-02-18 10:49:24 +0100

    Ajout des différents activités

---
+++ Polyjoule/src/com/example/polyjoule/live/LiveActivity.java
@@ -0,0 +1,19 @@
+package com.example.polyjoule.live;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LiveActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_news);
+	}
+	
+	public void onResume() {
+		super.onResume();
+		
+	}
+
+}
---
+++ Polyjoule/src/com/example/polyjoule/live/medias/MediasActivity.java
@@ -0,0 +1,19 @@
+package com.example.polyjoule.live.medias;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MediasActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_news);
+	}
+	
+	public void onResume() {
+		super.onResume();
+		
+	}
+
+}
