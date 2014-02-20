e81bfa31b463acf1be3151873c81e4f382b2f435
guinardA <guinard.arnaud@gmail.com>
2014-02-20 19:27:49 +0100

    Debut layout course

--- Polyjoule/src/com/example/polyjoule/course/CourseActivity.java
+++ Polyjoule/src/com/example/polyjoule/course/CourseActivity.java
@@ -1,5 +1,7 @@
 package com.example.polyjoule.course;
 
+import com.example.polyjoule.R;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -8,7 +10,7 @@ public class CourseActivity extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_news);
+		setContentView(R.layout.activity_course);
 	}
 	
 	public void onResume() {
