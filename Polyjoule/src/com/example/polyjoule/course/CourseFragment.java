1c100cbf661aee22365ad3c291d22f7e73ce777c
guinardA <guinard.arnaud@gmail.com>
2014-02-25 22:32:06 +0100

    Modification du course fragment

--- Polyjoule/src/com/example/polyjoule/course/CourseFragment.java
+++ Polyjoule/src/com/example/polyjoule/course/CourseFragment.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 public class CourseFragment extends Fragment{
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.activity_course, container, false);
+		View rootView = inflater.inflate(R.layout.fragment_course, container, false);
         
         return rootView;
 	}
